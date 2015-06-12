c47047ad14eb200efcd288d60441944f4104a246
egster <eweidema@gmail.com>
2015-06-12 16:39:21 -0700

    Improved drag & drop and added design prompt & title
    drag & drop changes
    Title
    Project management page

--- src/CrowdDesign/Scripts/Custom/EditProject.js
+++ src/CrowdDesign/Scripts/Custom/EditProject.js
@@ -4,12 +4,12 @@
         containment: "tbody",
         revert: "invalid",
         revertDuration: 200,
-        stack: ".divSketchDroppable",
+        stack: ".divSketchDroppable, .divNewSketchDroppable",
         delay: 150
     });
 
     $(".divDimensionDraggable").draggable({
-        helper: "original", 
+        helper: "original",
         opacity: 0.7,
         containment: "tbody",
         axis: "y",
@@ -19,16 +19,45 @@
     });
 
     $(".divSketchDroppable").droppable({
-        accept: ".divSketchDraggable",
+        accept: function (dropElem) {
+            if ($(this).attr("data-sketchId") != dropElem.attr("data-sketchId")) {
+                return true;
+            }
+        },
         hoverClass: "divHoveredSolutionDroppable",
         drop: sketchDropped
     });
 
+    $(".divNewSketchDroppable").droppable({
+        accept: ".divSketchDraggable",
+        hoverClass: "divHoveredNewSolutionDroppable",
+        drop: sketchDroppedOnNew
+    });
+
     $(".divDimensionDroppable").droppable({
-        accept: ".divDimensionDraggable, .divSketchDraggable",
+        accept: ".divDimensionDraggable",
         hoverClass: "divHoveredDimensionDroppable",
         drop: dimensionDropped
     });
+
+    $("#prompt").dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 300
+        },
+        hide: {
+            effect: "blind",
+            duration: 300
+        },
+        minWidth: 800,
+        resizable: false
+    });
+
+    $("#openPrompt").click(function () {
+        $("#prompt").dialog("open");
+    });
+
 });
 
 function sketchDropped(event, ui) {
@@ -50,56 +79,55 @@ function sketchDropped(event, ui) {
     });
 }
 
-function dimensionDropped(event, ui) {
+function sketchDroppedOnNew(event, ui) {
+    var sourceSketchId = ui.draggable.attr("data-sketchId");
     var targetDimensionId = $(this).attr("data-dimensionId");
 
-    if (ui.draggable.hasClass("divDimensionDraggable")) {
-        var sourceDimensionId = ui.draggable.attr("data-dimensionId");
+    $.ajax({
+        url: "/Sketch/MoveSketchToDimension?sourceSketchId=" + sourceSketchId + "&targetDimensionId=" + targetDimensionId,
+        type: "POST",
+        async: true,
+        processData: false,
+        cache: false,
+        success: function () {
+            location.reload();
+        },
+        error: function (eventArgs) {
+            alert("Failed to move the sketch\n\nError status: " + eventArgs.status + "\nError message: " + eventArgs.statusText);
+        }
+    })
+}
 
-        $("#divMergeConfirmationDialog").dialog({
-            resizable: false,
-            height: 300,
-            width: 400,
-            modal: true,
-            buttons: {
-                Yes: function () {
-                    $.ajax({
-                        url: "/Dimension/MergeDimensions?sourceDimensionId=" + sourceDimensionId + "&targetDimensionId=" + targetDimensionId,
-                        type: "POST",
-                        async: true,
-                        processData: false,
-                        cache: false,
-                        success: function () {
-                            location.reload();
-                        },
-                        error: function (eventArgs) {
-                            alert("Failed to merge the dimensions\n\nError status: " + eventArgs.status + "\nError message: " + eventArgs.statusText);
-                        }
-                    });
+function dimensionDropped(event, ui) {
+    var targetDimensionId = $(this).attr("data-dimensionId");
+    var sourceDimensionId = ui.draggable.attr("data-dimensionId");
 
-                    $(this).dialog("close");
-                },
-                No: function () {
-                    $(this).dialog("close");
-                }
-            }
-        });
-    }
-    else if (ui.draggable.hasClass("divSketchDraggable")) {
-        var sourceSketchId = ui.draggable.attr("data-sketchId");
+    $("#divMergeConfirmationDialog").dialog({
+        resizable: false,
+        height: 300,
+        width: 400,
+        modal: true,
+        buttons: {
+            Yes: function () {
+                $.ajax({
+                    url: "/Dimension/MergeDimensions?sourceDimensionId=" + sourceDimensionId + "&targetDimensionId=" + targetDimensionId,
+                    type: "POST",
+                    async: true,
+                    processData: false,
+                    cache: false,
+                    success: function () {
+                        location.reload();
+                    },
+                    error: function (eventArgs) {
+                        alert("Failed to merge the dimensions\n\nError status: " + eventArgs.status + "\nError message: " + eventArgs.statusText);
+                    }
+                });
 
-        $.ajax({
-            url: "/Sketch/MoveSketchToDimension?sourceSketchId=" + sourceSketchId + "&targetDimensionId=" + targetDimensionId,
-            type: "POST",
-            async: true,
-            processData: false,
-            cache: false,
-            success: function () {
-                location.reload();
+                $(this).dialog("close");
             },
-            error: function (eventArgs) {
-                alert("Failed to move the sketch\n\nError status: " + eventArgs.status + "\nError message: " + eventArgs.statusText);
+            No: function () {
+                $(this).dialog("close");
             }
-        });
-    }
+        }
+    });
 }
